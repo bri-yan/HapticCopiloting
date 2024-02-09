a908aa581e72f5f9e3f85dfeff55ab139e882fa9
gavin-pringle <gavinpringle00@gmail.com>
2024-02-08 18:34:31 -0800

    Added example ADC code to read ADC.
    Not in RTOS format! I need to learn how that works first

--- twiddlerino/src/main.cpp
+++ twiddlerino/src/main.cpp
@@ -1,15 +1,36 @@
 
 #include "app/twiddlerino.h"
 #include "Arduino.h"
+#include "Adafruit_ADS1X15.h"
+#include "SPI.h"
 
 const startup_type_t mode = startup_type_t::RUN_CONTROLLER_DEFAULT;
 
+Adafruit_ADS1115 ads;
+
 void setup() {
-  //everything is setup here
-  twiddlerino_setup(mode);
+  Serial.begin(9600);
+  
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+
+  ads.startADCReading(MUX_BY_CHANNEL[0], /*continuous=*/false);
 }
 
-void loop() {
-  //leave empty
-  //we are using free rtos tasks
+void loop()
+{
+  // If we don't have new data, skip this iteration.
+  if (ads.conversionComplete()) {
+    int16_t results = ads.getLastConversionResults();
+    float volts = ads.computeVolts(results);
+
+    Serial.print(volts); Serial.println("V");
+
+    // Start another conversion.
+    ads.startADCReading(MUX_BY_CHANNEL[0], /*continuous=*/false);
+  }
+
+  delay(10);
 }
