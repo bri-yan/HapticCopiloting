e971c6794afb9d0848d883009a68d84829c4dd02
sayeholoumi <sayeholoumi@yahoo.com>
2024-04-08 18:37:12 -0700

    added admittance

--- twiddlerino/python_tests/test_serial.py
+++ twiddlerino/python_tests/test_serial.py
@@ -6,12 +6,28 @@ import hashlib
 
 import numpy as np
 
+import serial
+
 import asyncio
 import serial_asyncio
 from serial_interface.serial_interface import TwidSerialInterfaceProtocol, TelemetryFrame, run_test, ControlType, CommandType, TwidID
 
 ###SERIAL CONFIGURATION for esp32
-SERIAL_PORT = '/dev/cu.Bluetooth-Incoming-Port'
+SERIAL_PORT = "/dev/tty.usbserial-1440"
+baudrate = 9600
+
+try:
+    # Open the serial port
+    ser = serial.Serial(SERIAL_PORT, baudrate)
+    
+    # Check if the port is open
+    if ser.is_open:
+        print(f"Serial port {SERIAL_PORT} is open.")
+    else:
+        print(f"Failed to open serial port {SERIAL_PORT}.")
+
+except Exception as e:
+    print(f"Error: {e}")
 
 async def serial_test(twid:TwidSerialInterfaceProtocol):
     tid = TwidID.TWID1_ID
